5ae83a951a8273797079474f6f91f1b7f47fb58e
sesha-codeprism <sesha.singaraju@codeprism.in>
2023-01-02 12:47:25 +0530

    DVR proxy changes

--- backend/dvrproxy/dvrproxy.ts
+++ backend/dvrproxy/dvrproxy.ts
@@ -1,3 +1,5 @@
+import { lang } from "../../src/config/constants";
+import { DefaultStore } from "../../src/utils/DiscoveryUtils";
 import { GLOBALS } from "../../src/utils/globals";
 import { GET } from "../utils/common/cloud";
 
@@ -7,10 +9,17 @@ export const DVRPROXY_URL = GLOBALS.bootstrapSelectors?.ServiceMap.Services.dvr;
 
 
 export const getViewableSubscriptionStems = async (uri: string, params: any) => {
-    const url = `${DVRPROXY_URL}v1/subscription-groups`;
+    const url = `${GLOBALS.bootstrapSelectors?.ServiceMap.Services.dvr}v1/subscription-groups/`;
+    const paramsObject = {
+        "$type-filter": "all",
+        "$state-filter": "viewable-scheduled",
+        "$orderby": "startdate",
+        "$lang": "en-US",
+        "storeId": DefaultStore.Id
+    }
     const response = await GET({
         url,
-        params: params,
+        params: paramsObject,
         headers: {
             Authorization: `OAUTH2 access_token="${GLOBALS.store.accessToken}"`,
         },
