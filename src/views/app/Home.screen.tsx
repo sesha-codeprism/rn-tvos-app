ad6efadbc575ef33ab5ef4151d32502cb1308721
sesha-codeprism <sesha.singaraju@codeprism.in>
2023-01-10 15:16:09 +0530

    Homescreen unused code removed

--- src/views/app/Home.screen.tsx
+++ src/views/app/Home.screen.tsx
@@ -26,8 +26,6 @@ import { Routes } from "../../config/navigation/RouterOutlet";
 import { Layout } from "../../utils/analytics/consts";
 import { ItemShowType } from "../../utils/common";
 import { SafeAreaView } from "react-native-safe-area-context";
-import useCurrentSlots from "../../customHooks/useCurrentSlots";
-
 interface HomeScreenProps {
   navigation: NativeStackNavigationProp<any>;
 }
@@ -42,7 +40,6 @@ const HomeScreen: React.FunctionComponent<HomeScreenProps> = (
   const [open, setOpen] = useState(false);
   const firstCardRef = useRef<TouchableOpacity>(null);
   const drawerRef: React.MutableRefObject<any> = useRef();
-  const currentSlotsData = useCurrentSlots();
 
   let feedTimeOut: any = null;
   let hubTimeOut: any = null;
