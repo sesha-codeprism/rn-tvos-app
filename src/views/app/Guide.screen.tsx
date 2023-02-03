a9d23097588c6bb9a227af4902542d24d1d5b1cf
sesha-codeprism <sesha.singaraju@codeprism.in>
2023-02-03 13:53:44 +0530

    #1423614 - Opening Details page from EPG guide

--- src/views/app/Guide.screen.tsx
+++ src/views/app/Guide.screen.tsx
@@ -5,15 +5,32 @@ import {
   NativeModules,
   requireNativeComponent,
 } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Routes } from "../../config/navigation/RouterOutlet";
+import { massageLiveFeed } from "../../utils/assetUtils";
+import { SourceType } from "../../utils/common";
 
 const MKGuide = requireNativeComponent("MKGuide");
 
-interface GuideScreenProps {}
+interface GuideScreenProps {
+  navigation: NativeStackNavigationProp<any>;
+  route: any;
+}
 
 const GuideScreen: React.FunctionComponent<GuideScreenProps> = (props) => {
+  const onUpdate = (event: { nativeEvent: { program: any } }) => {
+    // console.log("nativeevent", event);
+    console.log("program = ", event.nativeEvent.program);
+    const feed = massageLiveFeed(
+      [event.nativeEvent.program],
+      SourceType.LIVE
+    )[0];
+    feed["isFromEPG"] = true;
+    props.navigation.push(Routes.Details, { feed: feed });
+  };
   return (
     <View style={{ alignContent: "center", alignItems: "center", padding: 5 }}>
-      <MKGuide style={{ width: "100%", height: "100%" }} />
+      <MKGuide style={{ width: "100%", height: "100%" }} onUpdate={onUpdate} />
     </View>
   );
 };
