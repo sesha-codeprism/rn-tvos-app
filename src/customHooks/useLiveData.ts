98203f81e3405e38e455d74a6927a6008f4699ba
sesha-codeprism <sesha.singaraju@codeprism.in>
2023-02-24 13:22:05 +0530

    LiveData refetch interval update - set to 30 min from 3 hours

--- src/customHooks/useLiveData.ts
+++ src/customHooks/useLiveData.ts
@@ -6,8 +6,9 @@ import { GLOBALS } from '../utils/globals';
 import { buildNowNextMap, getChannelMap } from '../utils/live/LiveUtils';
 import useChannelRights from './useChannelRights';
 
-const intervalTimer = 10800000; // 3*(1000*60 * 60)
+// const intervalTimer = 10800000; // 3*(1000*60 * 60)
 
+const intervalTimer = 1800000; //30*(1000*60)
 const getLiveData = async (channelRightsInfo: any) => {
     try {
         return new Promise(async (resolve, reject) => {
