918b742f974d83c5933a947913ed2e28ad235788
asit-codeprism <asit.panda@codeprism.in>
2023-03-23 10:47:53 +0530

    [WIP] Implement to support UI notification in Apple TV client

---
+++ src/components/MFNotification/MFNotificationCard.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const MFNotificationCard = (props: Props) => {
+  return (
+    <View>
+      <Text>MFNotificationCard</Text>
+    </View>
+  )
+}
+
+export default MFNotificationCard
+
+const styles = StyleSheet.create({})
